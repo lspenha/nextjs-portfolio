1e8d0fcde9930891d7474fe2000d6de30805b6bd
Lucas Penha <dev.lspenha@gmail.com>
2023-08-01 10:29:52 -0300

    feat: update infos

--- app/page.tsx
+++ app/page.tsx
@@ -30,13 +30,13 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				Dev.lspenha
+				LSPenha
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-				Olá! 👋 Sou Lucas, um entusiasta do desenvolvimento Fullstack com uma paixão inabalável pela criação de soluções inovadoras e impactantes.
+				Olá! 👋 Sou Lucas, sou um desenvolvedor frontend apaixonado por criar interfaces de usuário dinâmicas e intuitivas utilizando ReactJS.
 				</h2>
 			</div>
 		</div>
--- util/stack.ts
+++ util/stack.ts
@@ -1,7 +1,7 @@
 export const fullStack = [
     {
         label: "Frontend",
-        technologies:["HTML", "CSS", "JavaScript (React)", "UI/UX design"]
+        technologies:["HTML", "CSS", "JavaScript e TypeScript (React)", "UI/UX design"]
     },
     {
         label: "Backend",
@@ -9,6 +9,10 @@ export const fullStack = [
     },
     {
         label: "Outras Ferramentas",
-        technologies:["Git"," RESTful APIs", "GraphQL", "Docker", "AWS"]
+        technologies:["Git"," RESTful APIs", "GraphQL", "Docker", "AWS", "Figma e Adobe XD"]
+    },
+    {
+        label: "Bibliotecas e Framework`s",
+        technologies:["Next.js E Nest.js","Tailwind CSS e Material ui","Formik, Yup e Zod", "Zustand e Context API", "Styled-components", "Axios e React Query", "Jest e Cypress.io", "Storybook", "CASL" , "Socket.io"]
     }
 ]
